2011812de139986fc75467c41d1be27232622a40
kogriffith <kevin.o.griffith@gmail.com>
2020-09-12 18:13:06 -0700

    react frontend setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import App from './components/App';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import * as serviceWorker from './serviceWorker';
 
+const link = createHttpLink({
+  uri: 'https://localhost:3000/graphql'
+});
+
+const client = new ApolloClient({
+  link: link,
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
