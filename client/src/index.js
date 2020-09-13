734a11e5a93b1344e71f2ac082a959a84f1b13a1
kogriffith <kevin.o.griffith@gmail.com>
2020-09-12 19:38:39 -0700

    remove unused css file import statement from index

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
 import App from './components/App';
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
