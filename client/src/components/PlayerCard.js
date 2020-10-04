34c95ebb3232ca3888870553b84fa3159c109fc5
kogriffith <kevin.o.griffith@gmail.com>
2020-10-03 21:44:26 -0700

    create PlayerCard

--- client/src/components/PlayerCard.js
+++ client/src/components/PlayerCard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {useQuery, gql} from '@apollo/client'
-
+import {useParams} from 'react-router-dom'
+import {Container, Row, Col, Spinner} from 'reactstrap'
 
 const GetPlayerInfo = gql `query GetPlayerInfo($id: ID!) {
   player(id: $id){
@@ -12,21 +13,32 @@ const GetPlayerInfo = gql `query GetPlayerInfo($id: ID!) {
 }`;
 
 const PlayerCard = () => {
-  
+  const {id} = useParams();
   const {data, loading} = useQuery(GetPlayerInfo, {
     variables: {
-      id: "1"
+      id: id
     }
   });
 
   return(
     <div>
-      {!loading &&
-        <>
-         <p>{data.player.name}</p>
-         <p>{data.player.winCount}</p>
-         <p>{data.player.id}</p>
-      </>
+      {loading ? (
+        <div>
+        </div>  
+      ) : (
+        <Container>
+          <Row>
+            <Col>
+              <h1>{data.player.name}</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+            <span>{data.player.winCount}</span>
+            </Col>
+          </Row>
+        </Container>
+      )
        }
     </div>
   )
